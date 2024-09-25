8439c91b50363cfc6e1a90c71de3108761b48b48
valeria.oliver <valeria.ao.veleiros@gmail.com>
2024-09-25 14:14:08 -0300

    att upl

--- js/script.js
+++ js/script.js
@@ -1,17 +1,33 @@
 const toggleTheme = document.getElementById("toggleTheme");
-
 const rootHtml = document.documentElement;
 
-/* função para alternar o tema */
-function changeTheme () {
-    const currentTheme = rootHtml.getAttribute("data-theme")
+const accordionHeaders = document.querySelectorAll(".accordion-header");
+//const menuLinks = document.querySelectorAll(".menu-link");
 
-    currentTheme === "dark" ? rootHtml.setAttribute("data-theme" ,"light") : rootHtml.setAttribute("data-theme", "dark");
+
+
+
+//função para alternar o tema 
+function changeTheme () {
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
 
     toggleTheme.classList.toggle("bi-sun");
     toggleTheme.classList.toggle("bi-moon-stars");
 
-}
+} 
 
 toggleTheme.addEventListener("click", changeTheme);
-/* FIM DA FUNÇÃO MUDAR O TEMA */
+//fim da função mudar o tema
+
+
+accordionHeaders.forEach(header => {
+    header.addEventListener("click", () => {
+        const accordionItem = header.parentElement;
+        const accordionActive = accordionItem.classList.contains("active");
+
+        accordionActive ? accordionItem.classList.remove("active") : accordionItem.classList.add("active");
+    })
+})
+
